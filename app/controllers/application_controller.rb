1b38e7131d3fd8fba2c3043fc2c4d9ac193e7cc3
David Falusi <falusi.david94@gmail.com>
2018-05-31 01:50:50 +0200

    Add unauthorized page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include SessionManagement
   protect_from_forgery with: :exception
   before_action :require_login
+
+  def unauthorized_page
+    render 'application/401', status: :unauthorized
+  end
 end
