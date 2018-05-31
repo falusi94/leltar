938acdf17e2c72f72a776aedc860ba99382df0e0
David Falusi <falusi.david94@gmail.com>
2018-05-31 04:17:03 +0200

    Fix current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
   end
   helper_method :current_user
 
