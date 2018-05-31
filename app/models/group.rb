9faf21b2bed81d5884241c5379f4bf20ea85cf9e
David Falusi <falusi.david94@gmail.com>
2018-05-31 02:42:41 +0200

    Add group selecting by rights

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   validates :name, uniqueness: true
   has_many :items
+  has_many :rights
 
   def self.by_name(name)
     Group.where(name: name).first
@@ -8,5 +9,17 @@ class Group < ApplicationRecord
 
   def self.exists?(name)
     !Group.where(name: name).empty?
+  end
+
+  def Group.can_read(user_id)
+    Group.select do |group|
+      group.rights.any? { |right| right.user_id == user_id }
+    end
+  end
+
+  def Group.can_write(user_id)
+    Group.select do |group|
+      group.rights.any? { |right| right.user_id == user_id && right.write }
+    end
   end
 end
