2ff92a04f9f25606b0deae6d8c7990fef7a9977c
David Falusi <falusi.david94@gmail.com>
2018-06-02 10:18:02 +0200

    Remove deprecated stuff

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,14 +5,6 @@ class Group < ApplicationRecord
   has_many :items
   has_many :rights
 
-  def self.by_name(name)
-    Group.where(name: name).first
-  end
-
-  def self.exists?(name)
-    !Group.where(name: name).empty?
-  end
-
   def Group.can_read(user_id)
     Group.select do |group|
       group.rights.any? { |right| right.user_id == user_id }
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ApplicationRecord
   searchkick
   scope :search_import, -> { includes(:group) }
 
-  VALID_STATUS = ['OK', 'Selejtezésre vár', 'Selejtezve', 'Utána kell járni', 'Elveszett']
+  # This is deprecated, but for info, keep the original values
+  # VALID_STATUS = ['OK', 'Selejtezésre vár', 'Selejtezve', 'Utána kell járni', 'Elveszett']
   enum state: [ :ok, :waiting_for_repair, :need_as_part, :waiting_for_scrapping,
                 :scrapped, :not_found, :at_group_member, :lost ]
   enum organization: [:ska, :svie]
@@ -11,10 +12,9 @@ class Item < ApplicationRecord
   has_paper_trail
   belongs_to :group
   has_many :photos
-  validates :name, length: {minimum: 2, too_short: 'Tul rovid nev'}
-  validates :description, length: {maximum: 300, too_long: 'Tul hosszu leiras'}
+  validates :name, length: {minimum: 2, too_short: 'Túl rövid név'}
+  validates :description, length: {maximum: 300, too_long: 'Túl hosszú leírás'}
   validates :group, presence: true, allow_nil: false
-  validates :status, inclusion: {in: VALID_STATUS}
 
   def search_data
     {
@@ -39,35 +39,17 @@ class Item < ApplicationRecord
 
   def to_a
     [self.id, self.name, self.description, self.group.name, self.purchase_date,
-     self.entry_date, self.last_check, self.status, self.old_number]
+     self.entry_date, self.last_check, self.state, self.old_number]
   end
 
   def validate_purchase_date
     if purchase_date.present? && purchase_date > Date.today
-      errors.add(:purchase_date, 'Jovobeni beszerzesi datum')
+      errors.add(:purchase_date, 'Jövőbeni beszerzési dátum')
     end
   end
 
   def picture_path(ix = 0)
     "/items/#{self.id}/photos/#{ix}"
-  end
-
-  def self.filter(query)
-    qs = query.split
-    res = Item.all
-    qs.each do |q|
-      tok = q.split(':')
-      if tok.length==1
-        pattern = "%#{tok[0]}%"
-        res = res.where('"name" LIKE ? OR "description" LIKE ? ', pattern, pattern)
-      elsif tok.length>1
-        field = tok[0]
-        if %w(group name description status).include?(field)
-          res = res.where("\"#{field}\" LIKE ?", "%#{tok[1]}%")
-        end
-      end
-    end
-    res
   end
 
 end
