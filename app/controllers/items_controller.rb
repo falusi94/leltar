3a18c99b215cf81e2f4e5868c17fda949e1f03e2
David Falusi <falusi.david94@gmail.com>
2018-06-02 10:00:01 +0200

    Change to new states

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -106,7 +106,7 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :purchase_date, :entry_date, :status, :old_number, :group_id, :picture)
+      params.require(:item).permit(:name, :description, :purchase_date, :entry_date, :state, :old_number, :group_id, :picture)
     end
 
     def picture_params
