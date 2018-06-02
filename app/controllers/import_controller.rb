fc44921fc70c828a98f036b08cdb6bff76635f2e
David Falusi <falusi.david94@gmail.com>
2018-06-02 06:01:05 +0200

    Fix import controller mismatched block

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -2,16 +2,15 @@ class ImportController < ApplicationController
 
   def upload_csv
     return unauthorized_page unless current_user.admin
-      if params[:csv_file]
-        @bad_items = ImportCsv.call(params[:csv_file].read.force_encoding('UTF-8'))
-        if @bad_items.size > 0
-          render :csv_errors
-        else
-          redirect_to '/items'
-        end
+    if params[:csv_file]
+      @bad_items = ImportCsv.call(params[:csv_file].read.force_encoding('UTF-8'))
+      if @bad_items.size > 0
+        render :csv_errors
       else
-        render inline: 'Unprocessable entity', status: :unprocessable_entity
+        redirect_to '/items'
       end
+    else
+      render inline: 'Unprocessable entity', status: :unprocessable_entity
     end
   end
 
