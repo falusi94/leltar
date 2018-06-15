fae4aa6b93c9754a54634f0800863126a66c97a8
David Falusi <falusi.david94@gmail.com>
2018-06-15 13:43:01 +0200

    Add dowload script for images

---
+++ scripts/download_from_sharepoint.py
@@ -0,0 +1,45 @@
+from sharepoint import SharePointSite, basic_auth_opener
+from urllib2 import BaseHandler, build_opener
+import os
+
+class CookieAuthHandler(BaseHandler):
+    def __init__(self, cookies):
+        self.cookies = cookies
+
+    def http_request(self, request):
+        for c in cookies:
+            request.add_header('Cookie', c)
+        return request
+    https_request = http_request
+
+
+server_url = "https://schonherzkollegium.sharepoint.com/"
+site_url = server_url + "teams/SKA/Leltar/"
+rtFa = ""
+FedAuth = ""
+auth_cookie = "rtFa=" + rtFa + "; FedAuth=" + FedAuth
+cookies = [auth_cookie]
+opener = build_opener(CookieAuthHandler(cookies))
+site = SharePointSite(site_url, opener)
+
+for sp_list in site.lists:
+    print sp_list.id, sp_list.meta['Title']
+c = 0
+for row in site.lists['{b4719275-6428-437b-92c9-284f23a2fa26}'].rows:
+    try:
+        for a in row.attachments:
+            name = a.url.split('/')[-1]
+            fn = row.Title + '_' + name
+            if not os.path.exists(fn):
+                try:
+                    fuckingrandomshit = a.open()
+                    with open(fn, 'w') as outf:
+                        outf.write(fuckingrandomshit.read())
+                    fuckingrandomshit.close()
+                except:
+                    print 'Fail %s' % a.Title
+            c += 1
+    except:
+        print 'Fail'
+
+print "Downloaded %d images" % c
