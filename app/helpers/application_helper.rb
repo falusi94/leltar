d38f31f981f55faaac220e52bd8cb14c8749222a
David Falusi <falusi.david94@gmail.com>
2018-10-07 17:12:30 +0200

    Add helper methods for button labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def back_label
+    create_label('arrow-left', 'Vissza')
+  end
+
+  def list_label
+    create_label('menu', 'Összes listázása')
+  end
+
+  def edit_label
+    create_label('pencil', 'Szerkesztés')
+  end
+
+  def new_label
+    create_label('plus', 'Új hozzáadása')
+  end
+
+  def delete_label
+    create_label('trash', 'Törlés')
+  end
+
+  private
+
+  def create_label(icon_name, label_text)
+    to_medium_tag = content_tag(:span, '', class: 'uk-hidden@m', 'uk-icon': icon_name)
+    from_medium_tag = content_tag(:span, label_text, class: 'uk-visible@m')
+    "#{to_medium_tag}#{from_medium_tag}".html_safe
+  end
 end
