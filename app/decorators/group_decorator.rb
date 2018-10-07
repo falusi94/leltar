00ce3b0c34b3f075cacc5ddec2a445db9cf578cf
David Falusi <falusi.david94@gmail.com>
2018-10-07 17:40:45 +0200

    Update groups' templates
    Make it responsive too

--- app/decorators/group_decorator.rb
+++ app/decorators/group_decorator.rb
@@ -5,12 +5,12 @@ class GroupDecorator < ApplicationDecorator
 
   def edit_group_button
     return unless current_user.admin
-    link_to('Szerkesztés', edit_group_path(group), class: 'uk-button uk-button-secondary')
+    link_to(edit_label, edit_group_path(group), class: 'uk-button uk-button-secondary')
   end
 
   def delete_group_button
     return unless current_user.admin
-    link_to('Törlés', group, method: :delete,
+    link_to(delete_label, group, method: :delete,
             data: { confirm: 'Biztosan törölni szeretnéd?' },
             class: 'uk-button uk-button-danger')
   end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,6 +1,6 @@
 module GroupsHelper
   def new_group_button
     return unless current_user.admin
-    link_to('Új kör', new_group_path, class: 'uk-button uk-button-primary uk-button-small')
+    link_to(new_label, new_group_path, class: 'uk-button uk-button-primary uk-button-small')
   end
 end
