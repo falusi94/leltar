ddba37ffc54ef3c5adc165bb59b82805e73b9d80
David Falusi <falusi.david94@gmail.com>
2018-10-26 23:11:36 +0200

    Fix images in asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( no_photo.gif loading.gif )
+Rails.application.config.assets.precompile += %w( no_photo.gif apple-touch-icon.png apple-touch-icon-precomposed.png favicon.png )
