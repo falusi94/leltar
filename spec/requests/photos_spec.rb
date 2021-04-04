76fb1d8bc64ffcd3e1b2f946f6cabfa10a635277
David Falusi <falusi.david94@gmail.com>
2021-04-04 21:45:57 +0100

    Add basic photos request spec

---
+++ spec/requests/photos_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+describe 'Photos' do
+  describe 'create photo' do
+    subject(:create_photo) do
+      file = Rack::Test::UploadedFile.new('spec/fixtures/files/dot.jpg', 'image/jpg')
+
+      post "/items/#{item.id}/photos", params: { photo: file }
+    end
+
+    let(:item) { create(:item) }
+
+    include_examples 'without user redirects to login'
+
+    context 'when the user can write group' do
+      it 'attaches the photo' do
+        login_admin
+
+        create_photo
+
+        expect(response).to redirect_to(edit_item_path(item))
+        expect(item.reload.photos).to be_present
+      end
+    end
+  end
+
+  describe 'delete photo' do
+    subject(:delete_photo) { delete "/items/#{item.id}/photos/#{item.photos.first.id}" }
+
+    let(:item) { create(:item, :with_photo) }
+
+    include_examples 'without user redirects to login'
+
+    context 'when the user can write group' do
+      it 'deletes the photo' do
+        login_admin
+
+        delete_photo
+
+        expect(response).to redirect_to(edit_item_path(item))
+        expect(item.reload.photos).to be_blank
+      end
+    end
+  end
+end
